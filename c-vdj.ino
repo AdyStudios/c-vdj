0b6f854d647cd1a69c2349478fd4a1772d3a4422
AdyStudios <karaadamka@gmail.com>
2024-04-03 13:57:13 +0200

    merge led control

--- c-vdj.ino
+++ c-vdj.ino
@@ -4,8 +4,15 @@
 // L E D 
 #define NUM_LEDS 8
 #define DATA_PIN 2
+#define LED_INTESITY 50
 
 CRGB leds[NUM_LEDS];
+
+bool waiting = true;
+
+CRGB randomColor() {
+    return CRGB(random(256), random(256), random(256));
+}
 
 // M I D I
 
@@ -25,21 +32,48 @@ NoteButtonMatrix<5, 5> buttonmatrix = {
   CHANNEL_1,    // channel and cable number
 };
 
-
-void setup(){
-    FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
-    Control_Surface.begin();
-    // Startup animation
-    for(int i; i < NUM_LEDS; i++){
+void ledSetup(){
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(LED_INTESITY);
+  for(int j = 0; j < 2 ; j++){
+    for(int i = 0; i < NUM_LEDS; i++){
         leds[i] = CRGB(0, 255, 255);
         FastLED.show();
-        delay(100);
+        delay(50);
         leds[i] = CRGB(0, 0, 0);
         FastLED.show();
-        delay(100);
+        
+    }
+    for (int i = NUM_LEDS; i --> 0; ){
+        leds[i] = CRGB(0, 255, 255);
+        FastLED.show();
+        delay(50);
+        leds[i] = CRGB(0, 0, 0);
+        FastLED.show();
+    }
   }
+}
+void setup(){
+    ledSetup();
+    Control_Surface.begin();
 }
 
 void loop() {
   Control_Surface.loop();
+  if(waiting){
+      waitingAnim();
+    }
+}
+
+void waitingAnim(){
+  for(int i = 0; i < NUM_LEDS; i++){
+      leds[i] = randomColor();
+      FastLED.show();
+      delay(300);
+      for (int j = 255; j >= 0; j--) {
+            leds[i].fadeToBlackBy(10); // Fade the LED
+            FastLED.show();
+            delay(5);
+        }
+    }
 }
--- led-control/led-control.ino
+++ led-control/led-control.ino
@@ -3,20 +3,56 @@
 #define NUM_LEDS 8
 #define DATA_PIN 2
 
+#define LED_INTESITY 50
+
+bool waiting = true;
+
+CRGB randomColor() {
+    return CRGB(random(256), random(256), random(256));
+}
+
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
 void setup() { 
     FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(LED_INTESITY);
+    for(int j = 0; j < 2 ; j++){
+
+    
+      for(int i = 0; i < NUM_LEDS; i++){
+          leds[i] = CRGB(0, 255, 255);
+          FastLED.show();
+          delay(50);
+          leds[i] = CRGB(0, 0, 0);
+          FastLED.show();
+          
+      }
+      for (int i = NUM_LEDS; i --> 0; ){
+          leds[i] = CRGB(0, 255, 255);
+          FastLED.show();
+          delay(50);
+          leds[i] = CRGB(0, 0, 0);
+          FastLED.show();
+      }
+    }
 }
 
 void loop() { 
-  for(int i; i < NUM_LEDS; i++){
-    leds[i] = CRGB(0, 255, 255);
-    FastLED.show();
-    delay(200);
-    leds[i] = CRGB(0, 0, 0);
-    FastLED.show();
-    delay(200);
-  }
+    if(waiting){
+      waitingAnim();
+    }
+}
+
+void waitingAnim(){
+  for(int i = 0; i < NUM_LEDS; i++){
+      leds[i] = randomColor();
+      FastLED.show();
+      delay(300);
+      for (int j = 255; j >= 0; j--) {
+            leds[i].fadeToBlackBy(10); // Fade the LED
+            FastLED.show();
+            delay(5);
+        }
+    }
 }
